21b2e0f3879facc4a0be1f1a25760d67cf2834c0
igorashs <igorashsirbu@gmail.com>
2021-01-07 15:59:53 +0200

    add withLayout

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import { GlobalStyle } from '../GlobalStyle';
+import { withLayout as withPageLayout } from '../shared/Layouts/Layout';
 
 function MyApp({ Component, pageProps }) {
+  const withLayout = Component.withLayout || withPageLayout;
+
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      {withLayout(<Component {...pageProps} />)}
     </>
   );
 }
