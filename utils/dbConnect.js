e357c9171f526ae1a9ba670a08fa7cab43942e5e
igorashs <igorashsirbu@gmail.com>
2021-01-11 18:24:45 +0200

    add dbConnect

---
+++ utils/dbConnect.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+async function dbConnect() {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+
+  return mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  });
+}
+
+export default dbConnect;
