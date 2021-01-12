50da24c4e67de2b4eabe1a862c2e867604142060
igorashs <igorashsirbu@gmail.com>
2021-01-12 20:12:49 +0200

    add validator

---
+++ utils/validator/index.js
@@ -0,0 +1,33 @@
+import {
+  userSignupSchema,
+  userAlreadyExistsDescription,
+  unauthorizedUserEmailDescription,
+  unauthorizedUserPasswordDescription,
+  userLoginSchema
+} from './schemas/user';
+import { ValidationError } from 'joi';
+
+export const validateUserSignup = (user) =>
+  userSignupSchema.validateAsync(user);
+
+export const validateUserLogin = (user) => userLoginSchema.validateAsync(user);
+
+export const throwUserAlreadyExists = () =>
+  throwValidationError(userAlreadyExistsDescription);
+
+export const throwUnauthorizedUserEmail = () =>
+  throwValidationError(unauthorizedUserEmailDescription);
+
+export const throwUnauthorizedUserPassword = () =>
+  throwValidationError(unauthorizedUserPasswordDescription);
+
+export const throwValidationError = ({ message, key }) => {
+  throw new ValidationError(message, [
+    {
+      message,
+      context: {
+        key
+      }
+    }
+  ]);
+};
---
+++ utils/validator/schemas/user.js
@@ -0,0 +1,57 @@
+import Joi from 'joi';
+
+export const userAlreadyExistsDescription = {
+  message: 'utilizatorul cu acest e-mail deja există',
+  key: 'email'
+};
+
+export const unauthorizedUserEmailDescription = {
+  message: 'e-mail greșit',
+  key: 'email'
+};
+
+export const unauthorizedUserPasswordDescription = {
+  message: 'parolă greșită',
+  key: 'password'
+};
+
+export const userSignupSchema = Joi.object({
+  name: Joi.string().trim().max(60).required().messages({
+    'string.base': 'numele trebuie sa fie de tip text',
+    'string.max': 'numele este prea lung',
+    'any.required': 'numele este obligatoriu'
+  }),
+
+  email: Joi.string().email().lowercase().required().messages({
+    'string.base': 'e-mailul trebuie sa fie de tip text',
+    'string.email': 'e-mailul nu este valid',
+    'any.required': 'e-mailul este obligatoriu'
+  }),
+
+  password: Joi.string()
+    .pattern(/^[\w\d\.]+$/u)
+    .min(8)
+    .max(128)
+    .required()
+    .messages({
+      'string.base': 'parola trebuie sa fie de tip text',
+      'string.min': 'parola trebuie să fie cel puțin din 8 caractere',
+      'string.max': 'parola este prea lungă',
+      'any.required': 'parola este obligatorie',
+      'string.pattern.base':
+        "parola este invalidă, sunt permise caractere alfanumerice și '.', '_'"
+    })
+});
+
+export const userLoginSchema = Joi.object({
+  email: Joi.string().email().lowercase().required().messages({
+    'string.base': 'e-mailul trebuie sa fie de tip text',
+    'string.email': 'e-mailul nu este valid',
+    'any.required': 'e-mailul este obligatoriu'
+  }),
+
+  password: Joi.string().required().messages({
+    'string.base': 'parola trebuie sa fie de tip text',
+    'any.required': 'parola este obligatorie'
+  })
+});
