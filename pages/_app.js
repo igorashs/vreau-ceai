80e1f844c051a3a9f997095cf7bbef486117d5f2
igorashs <igorashsirbu@gmail.com>
2021-01-13 10:12:57 +0200

    move Head

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
 import { GlobalStyle } from 'GlobalStyle';
 import { withLayout as withPageLayout } from '@/layouts/Layout';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   const withLayout = Component.withLayout || withPageLayout;
 
   return (
     <>
+      <Head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.svg" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <GlobalStyle />
       {withLayout(<Component {...pageProps} />)}
     </>
