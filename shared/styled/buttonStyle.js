4a6931ff4e64038cfd8b7e2d9b9ad9cc0458a51a
igorashs <igorashsirbu@gmail.com>
2021-01-13 09:49:46 +0200

    move buttonStyle

---
+++ shared/styled/buttonStyle.js
@@ -0,0 +1,15 @@
+import { css } from 'styled-components';
+
+export const buttonStyle = css`
+  padding: calc(var(--baseline) / 4) calc(var(--baseline) / 2);
+  border-radius: 4px;
+  border: 0;
+  color: var(--text-light);
+  background-color: var(--accent-dark);
+  box-shadow: 1px 1px 2px #00000066;
+  cursor: pointer;
+
+  :hover {
+    background-color: var(--accent);
+  }
+`;
