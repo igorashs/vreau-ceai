2da6b1ee31a66ea5145246e2c2ea089baccb27d7
igorashs <igorashsirbu@gmail.com>
2021-01-18 17:55:56 +0200

    add useSession

--- shared/ManagementMenu.js
+++ shared/ManagementMenu.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { StyledLink } from './StyledLink';
 import { DropDown } from './DropDown';
 import { managementLinks } from '@/utils/links';
+import { useSession } from 'contexts/SessionContext';
 
 const Wrapper = styled.div`
   h4 {
@@ -17,43 +18,53 @@ const Wrapper = styled.div`
 const { users, orders, products, categories } = managementLinks;
 
 export function ManagementMenu() {
+  const { user } = useSession();
+
   return (
     <Wrapper>
       <h4>Gestionare</h4>
 
-      <h5>Users management</h5>
-      <DropDown label="Utilizatori">
-        {users.map((l) => (
-          <li key={l.href}>
-            <StyledLink {...l} />
-          </li>
-        ))}
-      </DropDown>
+      {user?.isAdmin && (
+        <>
+          <h5>Users management</h5>
+          <DropDown label="Utilizatori">
+            {users.map((l) => (
+              <li key={l.href}>
+                <StyledLink {...l} />
+              </li>
+            ))}
+          </DropDown>
+        </>
+      )}
 
-      <h5>Shop management</h5>
-      <DropDown label="Comenzi">
-        {orders.map((l) => (
-          <li key={l.href}>
-            <StyledLink {...l} />
-          </li>
-        ))}
-      </DropDown>
+      {user?.isManager && (
+        <>
+          <h5>Shop management</h5>
+          <DropDown label="Comenzi">
+            {orders.map((l) => (
+              <li key={l.href}>
+                <StyledLink {...l} />
+              </li>
+            ))}
+          </DropDown>
 
-      <DropDown label="Produse">
-        {products.map((l) => (
-          <li key={l.href}>
-            <StyledLink {...l} />
-          </li>
-        ))}
-      </DropDown>
+          <DropDown label="Produse">
+            {products.map((l) => (
+              <li key={l.href}>
+                <StyledLink {...l} />
+              </li>
+            ))}
+          </DropDown>
 
-      <DropDown label="Categorii">
-        {categories.map((l) => (
-          <li key={l.href}>
-            <StyledLink {...l} />
-          </li>
-        ))}
-      </DropDown>
+          <DropDown label="Categorii">
+            {categories.map((l) => (
+              <li key={l.href}>
+                <StyledLink {...l} />
+              </li>
+            ))}
+          </DropDown>
+        </>
+      )}
     </Wrapper>
   );
 }
