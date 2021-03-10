339315a81cabf323250eb1062e1751774ebd7795
igorashs <igorashsirbu@gmail.com>
2021-03-10 14:29:38 +0200

    convert to ts

---
+++ shared/Leaves.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import LeavesSvg from 'assets/leaves.svg';
+
+const StyledFigure = styled.figure`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+interface LeavesProps {
+  height?: string;
+  width?: string;
+}
+
+export function Leaves({ height = '112', width = '112' }: LeavesProps) {
+  return (
+    <StyledFigure>
+      <LeavesSvg height={height} width={width} />
+    </StyledFigure>
+  );
+}
