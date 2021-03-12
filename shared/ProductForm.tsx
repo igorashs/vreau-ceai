9526b3343cd297b4b3df3d7eaa75c632f27aabdd
igorashs <igorashsirbu@gmail.com>
2021-03-12 09:17:02 +0200

    convert to ts

--- shared/ProductForm.tsx
+++ shared/ProductForm.tsx
@@ -1,26 +1,69 @@
+/* eslint-disable camelcase */
 import { useForm } from 'react-hook-form';
 import { joiResolver } from '@hookform/resolvers/dist/ie11/joi';
 import { productSchema } from '@/utils/validator/schemas/product';
 import { Form, FormAction } from '@/shared/Form';
 import { TextField } from '@/shared/TextField';
-import { CheckBox } from '@/shared/CheckBox';
-import { Button } from '@/shared/Button';
+import CheckBox from '@/shared/CheckBox';
+import Button from '@/shared/Button';
 import { Select } from '@/shared/Select';
 import { InputFile } from '@/shared/InputFile';
 import { Textarea } from '@/shared/Textarea';
 import { useEffect } from 'react';
 
-export function ProductForm({ onProductSubmit, categories, product }) {
+type Category = {
+  _id: string;
+  name: string;
+};
+
+type Product = {
+  _id: string;
+  name: string;
+  price: number;
+  quantity: number;
+  total_quantity: number;
+  description: string;
+  category_id: string;
+  recommend: boolean;
+  src?: FileList;
+};
+
+type ProductInputs = Omit<Product, '_id'>;
+
+type InputsErrors = Array<{
+  message: string;
+  name:
+    | 'name'
+    | 'price'
+    | 'quantity'
+    | 'total_quantity'
+    | 'description'
+    | 'category_id'
+    | 'recommend'
+    | 'src';
+}>;
+
+interface ProductFormProps {
+  onProductSubmit: (data: ProductInputs) => Promise<InputsErrors>;
+  categories: Array<Category>;
+  product: Product;
+}
+
+export function ProductForm({
+  onProductSubmit,
+  categories,
+  product,
+}: ProductFormProps) {
   const {
     register,
     handleSubmit,
     setError,
     reset,
     watch,
-    formState: { errors }
-  } = useForm({
+    formState: { errors },
+  } = useForm<ProductInputs>({
     mode: 'onChange',
-    resolver: joiResolver(productSchema)
+    resolver: joiResolver(productSchema),
   });
 
   const watchSrc = watch('src');
@@ -34,15 +77,15 @@ export function ProductForm({ onProductSubmit, categories, product }) {
       description: product?.description ?? '',
       category_id: product?.category_id,
       recommend: product?.recommend ?? false,
-      src: null
+      src: undefined,
     });
   }, [product, categories]);
 
-  const onSubmit = async (data) => {
-    const errors = await onProductSubmit(data);
+  const onSubmit = async (data: ProductInputs) => {
+    const submitErrors = await onProductSubmit(data);
 
-    if (errors) {
-      errors.forEach((error) => {
+    if (submitErrors) {
+      submitErrors.forEach((error) => {
         const { message, name } = error;
         setError(name, { message });
       });
@@ -52,7 +95,7 @@ export function ProductForm({ onProductSubmit, categories, product }) {
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <TextField
-        id={product && 'name_' + product._id}
+        id={product && `name_${product._id}`}
         name="name"
         label="nume produs"
         error={errors?.name?.message}
@@ -60,7 +103,7 @@ export function ProductForm({ onProductSubmit, categories, product }) {
         type="text"
       />
       <TextField
-        id={product && 'price_' + product._id}
+        id={product && `price_${product._id}`}
         name="price"
         label="preț (lei)"
         error={errors?.price?.message}
@@ -68,7 +111,7 @@ export function ProductForm({ onProductSubmit, categories, product }) {
         type="number"
       />
       <TextField
-        id={product && 'quantity_' + product._id}
+        id={product && `quantity_${product._id}`}
         name="quantity"
         label="preț per cantitate (g)"
         error={errors?.quantity?.message}
@@ -76,7 +119,7 @@ export function ProductForm({ onProductSubmit, categories, product }) {
         type="number"
       />
       <TextField
-        id={product && 'total_quantity_' + product._id}
+        id={product && `total_quantity_${product._id}`}
         name="total_quantity"
         label="cantitatea totală (g)"
         error={errors?.total_quantity?.message}
@@ -84,15 +127,15 @@ export function ProductForm({ onProductSubmit, categories, product }) {
         type="number"
       />
       <Textarea
-        id={product && 'description_' + product._id}
+        id={product && `description_${product._id}`}
         name="description"
         label="descriere"
         passRef={register}
         error={errors?.description?.message}
-        rows="7"
+        rows={7}
       />
       <Select
-        id={product && 'category_id_' + product._id}
+        id={product && `category_id_${product._id}`}
         name="category_id"
         label="categorie"
         passRef={register}
@@ -109,7 +152,7 @@ export function ProductForm({ onProductSubmit, categories, product }) {
         )}
       </Select>
       <InputFile
-        id={product && 'src_' + product._id}
+        id={product && `src_${product._id}`}
         name="src"
         label={`(max 1MB) imagine ${(watchSrc && watchSrc[0]?.name) || ''}`}
         passRef={register}
@@ -120,7 +163,7 @@ export function ProductForm({ onProductSubmit, categories, product }) {
       </InputFile>
       <FormAction justify="space-between">
         <CheckBox
-          id={product && 'recommend_' + product._id}
+          id={product && `recommend_${product._id}`}
           name="recommend"
           label="recomandată"
           passRef={register}
