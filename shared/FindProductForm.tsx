8039999315dd4af6e85621923b6ac559225a7809
igorashs <igorashsirbu@gmail.com>
2021-03-12 09:21:53 +0200

    convert to ts

--- shared/FindProductForm.tsx
+++ shared/FindProductForm.tsx
@@ -1,26 +1,38 @@
 import { TextField } from '@/shared/TextField';
-import { Button } from '@/shared/Button';
+import Button from '@/shared/Button';
 import { useForm } from 'react-hook-form';
 import { joiResolver } from '@hookform/resolvers/dist/ie11/joi';
 import { nameSchema } from '@/utils/validator/schemas/product';
 import { Form, FormAction } from '@/shared/Form';
 
-export function FindProductForm({ onFindProductSubmit }) {
+type ProductInputs = {
+  name: string;
+};
+
+type InputsErrors = Array<{ message: string; name: 'name' }>;
+
+interface FindProductFormProps {
+  onFindProductSubmit: (data: ProductInputs) => InputsErrors;
+}
+
+export const FindProductForm = ({
+  onFindProductSubmit,
+}: FindProductFormProps) => {
   const {
     register,
     handleSubmit,
     setError,
-    formState: { errors }
+    formState: { errors },
   } = useForm({
     mode: 'onChange',
-    resolver: joiResolver(nameSchema)
+    resolver: joiResolver(nameSchema),
   });
 
-  const onSubmit = async (data) => {
-    const errors = await onFindProductSubmit(data);
+  const onSubmit = async (data: ProductInputs) => {
+    const submitErrors = await onFindProductSubmit(data);
 
-    if (errors) {
-      errors.forEach((error) => {
+    if (submitErrors) {
+      submitErrors.forEach((error) => {
         const { message, name } = error;
         setError(name, { message });
       });
@@ -41,4 +53,4 @@ export function FindProductForm({ onFindProductSubmit }) {
       </FormAction>
     </Form>
   );
-}
+};
