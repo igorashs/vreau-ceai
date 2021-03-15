abe983e136095520c84c263a93437a2ab251a767
igorashs <igorashsirbu@gmail.com>
2021-03-15 10:50:24 +0200

    convert to ts

--- contexts/CartContext.tsx
+++ contexts/CartContext.tsx
@@ -3,19 +3,55 @@ import {
   useContext,
   useEffect,
   useReducer,
-  useRef
+  useRef,
 } from 'react';
 
-const CartContext = createContext();
-const CartDispatchContext = createContext();
-
-const INITIAL = {
-  items: [],
-  itemsCount: 0,
-  totalPrice: 0
+type Product = {
+  name: string;
+  price: number;
 };
 
-function cartReducer(state, action) {
+interface Cart {
+  items: Array<{ product: Product; count: number }>;
+  itemsCount: number;
+  totalPrice: number;
+  updated: boolean;
+}
+
+type ACTION_TYPE =
+  | {
+      type: 'add-item';
+      payload: { product: Product; count: number };
+    }
+  | {
+      type: 'update-item';
+      payload: { product: Product; count: number };
+    }
+  | {
+      type: 'remove-item';
+      payload: { product: Product };
+    }
+  | {
+      type: 'clear-cart';
+    }
+  | {
+      type: 'update-cart';
+      payload: Cart;
+    };
+
+const CartContext = createContext<Cart>({} as Cart);
+const CartDispatchContext = createContext<React.Dispatch<ACTION_TYPE>>(
+  {} as React.Dispatch<ACTION_TYPE>,
+);
+
+const INITIAL: Cart = {
+  items: [],
+  itemsCount: 0,
+  totalPrice: 0,
+  updated: false,
+};
+
+const cartReducer = (state: Cart, action: ACTION_TYPE): Cart => {
   switch (action.type) {
     case 'add-item': {
       const { product, count } = action.payload;
@@ -24,22 +60,23 @@ function cartReducer(state, action) {
       // check if item exists in cart
       if (item) {
         const items = state.items.map((i) =>
-          i === item ? { ...i, count: i.count + count } : i
+          i === item ? { ...i, count: i.count + count } : i,
         );
 
         return {
           items,
           itemsCount: state.itemsCount + count,
-          totalPrice: state.totalPrice + count * product.price
-        };
-      } else {
-        // add item in cart
-        return {
-          items: [...state.items, { product, count }],
-          itemsCount: state.itemsCount + count,
-          totalPrice: state.totalPrice + count * product.price
+          totalPrice: state.totalPrice + count * product.price,
+          updated: state.updated,
         };
       }
+      // add item in cart
+      return {
+        items: [...state.items, { product, count }],
+        itemsCount: state.itemsCount + count,
+        totalPrice: state.totalPrice + count * product.price,
+        updated: state.updated,
+      };
     }
 
     case 'update-item': {
@@ -49,13 +86,14 @@ function cartReducer(state, action) {
       return item && +count > 0
         ? {
             items: state.items.map((i) =>
-              i.product.name === product.name ? { ...i, count } : i
+              i.product.name === product.name ? { ...i, count } : i,
             ),
             itemsCount: state.itemsCount - item.count + count,
             totalPrice:
               state.totalPrice -
               item.count * item.product.price +
-              count * product.price
+              count * product.price,
+            updated: state.updated,
           }
         : state;
     }
@@ -68,7 +106,8 @@ function cartReducer(state, action) {
         ? {
             items: state.items.filter((i) => i !== item),
             itemsCount: state.itemsCount - item.count,
-            totalPrice: state.totalPrice - item.count * item.product.price
+            totalPrice: state.totalPrice - item.count * item.product.price,
+            updated: state.updated,
           }
         : state;
     }
@@ -80,30 +119,28 @@ function cartReducer(state, action) {
     case 'update-cart': {
       return {
         ...action.payload,
-        updated: true
+        updated: true,
       };
     }
 
     default:
-      throw new Error(`Unhandled action type ${action.type}`);
+      throw new Error(`Unhandled action type`);
   }
-}
+};
 
-function CartProvider({ children }) {
+const CartProvider: React.FC = ({ children }) => {
   const firstLoad = useRef(true);
   const [state, dispatch] = useReducer(cartReducer, { ...INITIAL });
 
   useEffect(() => {
     if (firstLoad.current) {
-      const cart = JSON.parse(localStorage.getItem('cart'));
+      const cart = JSON.parse(localStorage.getItem('cart') || '');
       firstLoad.current = false;
 
       if (cart)
         dispatch({ type: 'update-cart', payload: { ...INITIAL, ...cart } });
-    } else {
-      if (!state.updated) {
-        localStorage.setItem('cart', JSON.stringify(state));
-      }
+    } else if (!state.updated) {
+      localStorage.setItem('cart', JSON.stringify(state));
     }
   }, [state]);
 
@@ -114,9 +151,9 @@ function CartProvider({ children }) {
       </CartDispatchContext.Provider>
     </CartContext.Provider>
   );
-}
+};
 
-function useCartState() {
+const useCartState = () => {
   const context = useContext(CartContext);
 
   if (!context) {
@@ -124,9 +161,9 @@ function useCartState() {
   }
 
   return context;
-}
+};
 
-function useCartDispatch() {
+const useCartDispatch = () => {
   const context = useContext(CartDispatchContext);
 
   if (!context) {
@@ -134,10 +171,10 @@ function useCartDispatch() {
   }
 
   return context;
-}
+};
 
-function useCart() {
+const useCart = () => {
   return [useCartState(), useCartDispatch()];
-}
+};
 
 export { CartProvider, useCart, useCartState, useCartDispatch };
