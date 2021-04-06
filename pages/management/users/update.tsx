59851d442bf4327ff36c6c106b4b1bfe4896e1a2
igorashs <igorashsirbu@gmail.com>
2021-04-06 14:35:47 +0300

    convert to ts

--- pages/management/users/update.tsx
+++ pages/management/users/update.tsx
@@ -2,41 +2,44 @@ import { withManagementStoreLayout } from '@/layouts/StoreLayout';
 import { findUser, updateUserManagerPermission } from 'services/ceaiApi';
 import { useState } from 'react';
 import { Label } from '@/shared/Label';
-import { withSession } from '@/utils/withSession';
+import { withSessionServerSideProps } from '@/utils/withSession';
 import Head from 'next/head';
 import { FindUserForm } from '@/shared/FindUserForm';
 import { UserForm } from '@/shared/UserForm';
+import { LabelMessage, User, UserEmail, UserPermissions } from 'types';
 
 export default function Update() {
-  const [dbUser, setDbUser] = useState();
-  const [label, setLabel] = useState();
+  const [dbUser, setDbUser] = useState<User | null>();
+  const [label, setLabel] = useState<LabelMessage>();
 
-  const handleFindUserSubmit = async ({ email }) => {
-    const res = await findUser(email);
+  const handleFindUserSubmit = async (data: UserEmail) => {
+    const res = await findUser(data);
 
     if (res.success) {
       setLabel({ success: true, message: 'utilizatorul a fost găsit' });
       setDbUser({ ...res.user });
-    } else {
-      setLabel({ success: false, message: 'utilizatorul nu a fost găsit :(' });
-      setDbUser(null);
-      return res.errors;
+      return [];
     }
+    setLabel({ success: false, message: 'utilizatorul nu a fost găsit :(' });
+    setDbUser(null);
+    return res.errors;
   };
 
-  const handleUserSubmit = async ({ isManager }) => {
-    const res = await updateUserManagerPermission(dbUser._id, isManager);
+  const handleUserSubmit = async (data: UserPermissions) => {
+    if (!dbUser) return;
+
+    const res = await updateUserManagerPermission(dbUser._id, data);
 
     if (res.success) {
       setDbUser({ ...res.user });
       setLabel({
         success: true,
-        message: 'utilizatorul a fost modificat :)'
+        message: 'utilizatorul a fost modificat :)',
       });
     } else {
       setLabel({
         success: false,
-        message: 'ceva nu a mers bine :('
+        message: 'ceva nu a mers bine :(',
       });
     }
   };
@@ -62,19 +65,21 @@ export default function Update() {
   );
 }
 
-export const getServerSideProps = withSession(async ({ req }) => {
-  const { isAuth, user } = req.session;
+export const getServerSideProps = withSessionServerSideProps(
+  async ({ req }) => {
+    const { isAuth, user } = req.session;
 
-  if (!isAuth || !user.isAdmin) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false
-      }
-    };
-  }
+    if (!isAuth || !user?.isAdmin) {
+      return {
+        redirect: {
+          destination: '/',
+          permanent: false,
+        },
+      };
+    }
 
-  return { props: {} };
-});
+    return { props: {} };
+  },
+);
 
 Update.withLayout = withManagementStoreLayout;
