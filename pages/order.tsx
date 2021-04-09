3c3e888668cebcbf44e6edc88b142dcc1b078e0f
igorashs <igorashsirbu@gmail.com>
2021-04-09 12:13:11 +0300

    convert to ts

--- pages/order.tsx
+++ pages/order.tsx
@@ -2,16 +2,17 @@ import { withBaseLayout } from '@/layouts/BaseLayout';
 import Head from 'next/head';
 import styled from 'styled-components';
 import { Form, FormAction } from '@/shared/Form';
-import { withSession } from '@/utils/withSession';
+import { withSessionServerSideProps } from '@/utils/withSession';
 import { useForm } from 'react-hook-form';
 import { joiResolver } from '@hookform/resolvers/dist/ie11/joi';
 import { orderSubmitSchema } from '@/utils/validator/schemas/order';
 import { TextField } from '@/shared/TextField';
-import { Button } from '@/shared/Button';
+import Button from '@/shared/Button';
 import { useCart } from 'contexts/CartContext';
 import { createOrder } from 'services/ceaiApi';
 import { useState } from 'react';
 import { Label } from '@/shared/Label';
+import { LabelMessage, OrderSubmit } from 'types';
 
 const FormWrapper = styled.div`
   max-width: var(--max-input-width);
@@ -28,30 +29,30 @@ export default function Order() {
     register,
     handleSubmit,
     setError,
-    formState: { errors }
+    formState: { errors },
   } = useForm({
     mode: 'onChange',
-    resolver: joiResolver(orderSubmitSchema)
+    resolver: joiResolver(orderSubmitSchema),
   });
 
   const [cart, cartDispatch] = useCart();
-  const [label, setLabel] = useState();
+  const [label, setLabel] = useState<LabelMessage>();
 
-  const onSubmit = async (data) => {
+  const onSubmit = async (data: OrderSubmit) => {
     const items = cart.items.map(({ product, count }) => ({
       product_id: product._id,
-      count
+      count,
     }));
 
     const res = await createOrder({
       info: data,
-      items
+      items,
     });
 
     if (res.success) {
       setLabel({
         success: true,
-        message: res.message
+        message: res.message,
       });
 
       cartDispatch({ type: 'clear-cart' });
@@ -60,13 +61,12 @@ export default function Order() {
     if (res?.errors) {
       res.errors.forEach((error) => {
         const { message, name } = error;
-
-        setError(name, { message });
+        if (name) setError(name, { message });
       });
     } else if (!res.success) {
       setLabel({
         success: false,
-        message: 'comanda nu a fost trimisă, goliți coșul și încercați din nou'
+        message: 'comanda nu a fost trimisă, goliți coșul și încercați din nou',
       });
     }
   };
@@ -120,19 +120,21 @@ export default function Order() {
   );
 }
 
-export const getServerSideProps = withSession(async ({ req }) => {
-  const { isAuth } = req.session;
+export const getServerSideProps = withSessionServerSideProps(
+  async ({ req }) => {
+    const { isAuth } = req.session;
 
-  if (!isAuth) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false
-      }
-    };
-  }
+    if (!isAuth) {
+      return {
+        redirect: {
+          destination: '/',
+          permanent: false,
+        },
+      };
+    }
 
-  return { props: {} };
-});
+    return { props: {} };
+  },
+);
 
 Order.withLayout = withBaseLayout;
