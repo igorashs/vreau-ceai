7eefa9a50ed69fbc4a350584e0ed66674023a8bc
igorashs <igorashsirbu@gmail.com>
2021-04-12 09:36:44 +0300

    update getQueryElements

--- utils/getQueryElements.ts
+++ utils/getQueryElements.ts
@@ -1,11 +1,16 @@
 export const getQueryElement = (value: string | string[]) =>
   typeof value === 'string' ? value : value[0];
 
-export const getQueryElements = (query: { [key: string]: string | string[] }) =>
+export const getQueryElements = (query: {
+  [key: string]: string | string[] | undefined;
+}) =>
   Object.entries(query).reduce(
-    (elements: { [key: string]: string }, [key, value]) => ({
-      ...elements,
-      [key]: getQueryElement(value),
-    }),
+    (elements: { [key: string]: string }, [key, value]) =>
+      value
+        ? {
+            ...elements,
+            [key]: getQueryElement(value),
+          }
+        : elements,
     {},
   );
