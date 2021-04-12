b528546c74c1c9c385ef49469e6e26ab126b5e5f
igorashs <igorashsirbu@gmail.com>
2021-04-12 13:04:07 +0300

    refactoring getProductFormData

---
+++ utils/getProductFormData.ts
@@ -0,0 +1,16 @@
+import { ProductFields } from 'types';
+
+export const getProductFormData = (data: ProductFields & { src: FileList }) => {
+  const formData = new FormData();
+
+  Object.entries(data).forEach(([name, value]) => {
+    if (name === 'src') {
+      if (data.src[0]) formData.append(name, data.src[0]);
+      return;
+    }
+
+    formData.append(name, value.toString());
+  });
+
+  return formData;
+};
