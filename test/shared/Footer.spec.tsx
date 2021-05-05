555b4d3e754a306d9455d42c083450a6a31287f5
igorashs <igorashsirbu@gmail.com>
2021-05-05 10:37:14 +0300

    add new expect

--- test/shared/Footer.spec.tsx
+++ test/shared/Footer.spec.tsx
@@ -1,10 +1,12 @@
 import { render } from '@/utils/test-utils';
 import Footer from '@/shared/Footer';
+import { footerBlockLinksList } from '@/utils/links';
 
 describe('Footer', () => {
   it('renders correctly', () => {
-    const { container } = render(<Footer />);
+    const { container, getByText } = render(<Footer />);
 
+    expect(getByText(footerBlockLinksList[0].text)).toBeVisible();
     expect(container.firstChild).toMatchSnapshot();
   });
 });
