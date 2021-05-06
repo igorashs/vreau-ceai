bcc54ff8ba1aaa1a423e5bcc540c134da3e4e987
igorashs <igorashsirbu@gmail.com>
2021-05-06 15:16:54 +0300

    add renderWithSession

--- utils/test-utils.tsx
+++ utils/test-utils.tsx
@@ -1,5 +1,7 @@
 import { render, RenderOptions } from '@testing-library/react';
 import GlobalStyle from 'GlobalStyle';
+import { UserSession } from 'types';
+import { SessionProvider } from 'contexts/SessionContext';
 
 const Wrapper: React.FC = ({ children }) => (
   <>
@@ -8,11 +10,32 @@ const Wrapper: React.FC = ({ children }) => (
   </>
 );
 
-const customRender = (
+type Options = Omit<RenderOptions, 'queries'>;
+
+const customRender = (ui: React.ReactElement, options?: Options) =>
+  render(ui, { wrapper: Wrapper, ...options });
+
+const renderWithSession = (
   ui: React.ReactElement,
-  options?: Omit<RenderOptions, 'queries'>,
-) => render(ui, { wrapper: Wrapper, ...options });
+  session: UserSession,
+  options?: Options,
+) => {
+  const { rerender, ...all } = customRender(
+    <SessionProvider session={session}>{ui}</SessionProvider>,
+    options,
+  );
+
+  const rerenderWithSession = (
+    rerenderUI: React.ReactElement,
+    rerenderSession: UserSession,
+  ) =>
+    rerender(
+      <SessionProvider session={rerenderSession}>{rerenderUI}</SessionProvider>,
+    );
+
+  return { ...all, rerender, rerenderWithSession };
+};
 
 export * from '@testing-library/react';
 
-export { customRender as render };
+export { customRender as render, renderWithSession };
