11bc457304feef8f2f226e6c85d4d1bcaef329b6
igorashs <igorashsirbu@gmail.com>
2021-05-07 09:41:24 +0300

    split error messages

--- utils/validator/schemas/category.ts
+++ utils/validator/schemas/category.ts
@@ -1,12 +1,19 @@
 import Joi from 'joi';
 import { CategoryName } from 'types';
 
+export const categoryMessages = {
+  required: 'numele este obligatoriu',
+  max: 'numele este prea lung',
+  type: 'numele trebuie sa fie de tip text',
+  exists: 'Categoria cu acest nume deja există',
+};
+
 // eslint-disable-next-line import/prefer-default-export
 export const categorySchema = Joi.object<CategoryName>({
   name: Joi.string().trim().max(30).required().messages({
-    'string.base': 'numele trebuie sa fie de tip text',
-    'string.max': 'numele este prea lung',
-    'string.empty': 'numele este obligatoriu',
-    'any.required': 'numele este obligatoriu',
+    'string.base': categoryMessages.type,
+    'string.max': categoryMessages.max,
+    'string.empty': categoryMessages.required,
+    'any.required': categoryMessages.required,
   }),
 });
