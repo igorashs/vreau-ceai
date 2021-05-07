484b0077a1ccbeaae4c932c13732d280bd0691dc
igorashs <igorashsirbu@gmail.com>
2021-05-07 11:10:55 +0300

    split order errors messages

--- utils/validator/schemas/order.ts
+++ utils/validator/schemas/order.ts
@@ -1,19 +1,41 @@
 import Joi from 'joi';
 import { OrderItem, OrderNumber, OrderStatus, OrderSubmit } from 'types';
 
+export const orderMessages = {
+  tel: {
+    invalid: 'telefonul este invalid',
+    required: 'telefonul este obligatoriu',
+  },
+
+  address: {
+    max: 'adresa este prea lungă',
+    required: 'adresa este obligatorie',
+  },
+
+  number: {
+    length: 'numărul trebuie să aibă exact 16 caractere',
+    required: 'numărul este obligatoriu',
+  },
+
+  status: {
+    invalid: 'status greșit',
+    required: 'statusul este obligatoriu',
+  },
+};
+
 export const orderSubmitSchema = Joi.object<OrderSubmit>({
   tel: Joi.string()
     .pattern(/^0\d{8}$/)
     .required()
     .messages({
-      'string.empty': 'telefonul este obligatoriu',
-      'any.required': 'telefonul este obligatoriu',
-      'string.pattern.base': 'telefonul este invalid',
+      'string.empty': orderMessages.tel.required,
+      'any.required': orderMessages.tel.required,
+      'string.pattern.base': orderMessages.tel.invalid,
     }),
   address: Joi.string().trim().max(200).messages({
-    'string.empty': 'adresa este obligatorie',
-    'any.required': 'adresa este obligatorie',
-    'string.max': 'adresa este prea lungă',
+    'string.empty': orderMessages.address.required,
+    'any.required': orderMessages.address.required,
+    'string.max': orderMessages.address.max,
   }),
 });
 
@@ -26,9 +48,9 @@ export const orderItemsSchema = Joi.array().items(
 
 export const orderNumberSchema = Joi.object<OrderNumber>({
   number: Joi.string().length(16).required().messages({
-    'string.length': 'numărul trebuie să aibă exact 16 caractere',
-    'string.empty': 'numărul este obligatoriu',
-    'any.required': 'numărul este obligatoriu',
+    'string.length': orderMessages.number.length,
+    'string.empty': orderMessages.number.required,
+    'any.required': orderMessages.number.required,
   }),
 });
 
@@ -37,8 +59,8 @@ export const orderStatusSchema = Joi.object<OrderStatus>({
     .valid('processing', 'inDelivery', 'canceled', 'completed')
     .required()
     .messages({
-      'string.empty': 'statusul este obligatoriu',
-      'any.required': 'statusul este obligatoriu',
-      'any.only': 'status greșit',
+      'string.empty': orderMessages.status.required,
+      'any.required': orderMessages.status.required,
+      'any.only': orderMessages.status.invalid,
     }),
 });
