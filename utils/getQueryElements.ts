a847b4e0d79527c4713aef2bd47c49cb5f0ed852
igorashs <igorashsirbu@gmail.com>
2021-05-14 16:54:29 +0300

    add some useful comments

--- utils/getQueryElements.ts
+++ utils/getQueryElements.ts
@@ -1,6 +1,8 @@
+// returns the string value or first string element if it is an array
 export const getQueryElement = (value: string | string[]) =>
   typeof value === 'string' ? value : value[0];
 
+// returns string values || first string element of an array omitting undefined values
 export const getQueryElements = (query: {
   [key: string]: string | string[] | undefined;
 }) =>
