266953c14c91e896fc84ced324deda4cf578e8e2
igorashs <igorashsirbu@gmail.com>
2021-05-19 13:45:08 +0300

    reactor with ApiRouteService

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -1,148 +1,153 @@
 import dbConnect from '@/utils/dbConnect';
 import { withSessionApi } from '@/utils/withSession';
-import * as validator from '@/utils/validator';
-import { getQueryElements } from '@/utils/getQueryElements';
 import getOrderFilters from '@/utils/getOrderFilters';
 import OrderService from 'services/OrderService';
 import UserService from 'services/UserService';
+import ApiRouteService from 'services/ApiRouteService';
+import { Order } from '@/models/Order';
 
 export default withSessionApi(async function handler(req, res) {
   await dbConnect();
+  const routeService = new ApiRouteService(req, res);
 
   switch (req.method) {
     case 'GET':
-      try {
-        const { isAuth, user } = req.session;
-
-        // respond 401 Unauthorized if user is not authenticated
-        if (!isAuth || !user) {
-          res.status(401).json({ success: false, message: 'Unauthorized' });
-
-          return;
-        }
-
-        const {
-          search,
-          filters: filtersQuery,
-          byUserId,
-          byUserEmail,
-          offset,
-          limit,
-        } = getQueryElements(req.query);
-
-        const { matchFilter, sortFilter } = getOrderFilters(filtersQuery);
-        const options = {
-          limit: +limit || 3,
-          skip: +offset || 0,
-          sort: sortFilter,
-        };
-
-        // get session user orders
-        if (byUserId) {
-          const dbOrders = await OrderService.queryOrdersByUserId(
-            user._id,
-            matchFilter,
-            'number items total_price status orderedAt completedAt',
-            options,
-          );
-
-          if (dbOrders.length) {
-            const count = await OrderService.countOrdersByUserId(
-              user._id,
-              matchFilter,
-            );
-
-            res.status(200).json({ success: true, orders: dbOrders, count });
-          } else {
-            res.status(400).json({ success: false, message: 'Not Found' });
-          }
-        } else {
-          // respond 401 Unauthorized if user doesn't have permission
-          if (!user.isAdmin && !user.isManager) {
-            res.status(401).json({ success: false, message: 'Unauthorized' });
-
-            return;
-          }
-
-          if (search) {
-            // ? I think this is not used anywhere o?
-            if (byUserEmail) {
-              // find user orders by user email
-
-              // respond 400 Validation Error if the name is invalid
-              // Respond 400 Validation Error if user doesn't exist
-              const dbUser = await UserService.findUser(search, '_id');
-
-              const dbOrders = await OrderService.listOrdersByUserId(
-                dbUser._id,
-                matchFilter,
-                null,
-                options,
-              ).populate({
-                path: 'user',
-                select: 'name email',
-              });
-
-              if (dbOrders.length) {
-                const count = await OrderService.countOrdersByUserId(
-                  user._id,
-                  matchFilter,
-                );
-
-                res
-                  .status(200)
-                  .json({ success: true, orders: dbOrders, count });
-              } else {
-                res.status(400).json({ success: false, message: 'Not Found' });
-              }
-            } else {
-              // find order by its number
-              const dbOrder = await OrderService.queryOrder(search);
-
-              if (dbOrder) {
-                res.status(200).json({ success: true, order: dbOrder });
-              } else {
-                res.status(404).json({ success: false, message: 'Not Found' });
-              }
-            }
-          } else {
-            // find orders with filters
-            const dbOrders = await OrderService.listOrders(
-              matchFilter,
-              null,
-              options,
-            ).populate({
-              path: 'user',
-              select: 'name email',
-            });
-
-            if (dbOrders.length) {
-              const count = await OrderService.countOrders(matchFilter);
-
-              res.status(200).json({ success: true, orders: dbOrders, count });
-            } else {
-              res.status(400).json({ success: false, message: 'Not Found' });
-            }
-          }
-        }
-      } catch (error) {
-        const details = validator.getValidationErrorDetails(error);
-
-        if (details) {
-          res.status(400).json({
-            success: false,
-            message: 'Validation Errors',
-            errors: details,
-          });
-        } else {
-          res.status(400).json({ success: false, message: 'Bad Request' });
-        }
-      }
+      await handleGet(routeService);
 
       break;
 
     default:
-      res.status(400).json({ success: false, message: 'Bad Request' });
+      routeService.resMethodNotAllowed(['GET'], req.method);
+
       break;
   }
 });
+
+/**
+ * Get session user orders (by userId) ||
+ * Get user orders by email ||
+ * Get order by its number ||
+ * Get orders with matched filters
+ */
+const handleGet = async (
+  routeService: ApiRouteService<{
+    orders?: Order[];
+    count?: number;
+    order?: Order;
+  }>,
+) => {
+  try {
+    // Respond 401 Unauthorized if user is not authorized
+    if (!routeService.isAuthorized()) return;
+
+    const {
+      search,
+      filters: filtersQuery,
+      byUserId,
+      byUserEmail,
+      offset,
+      limit,
+    } = routeService.getQuery();
+
+    const { matchFilter, sortFilter } = getOrderFilters(filtersQuery);
+    const options = {
+      limit: +limit || 3,
+      skip: +offset || 0,
+      sort: sortFilter,
+    };
+
+    // get session user orders
+    if (byUserId) {
+      const { user } = routeService.getUserSession();
+      const dbOrders = await OrderService.queryOrdersByUserId(
+        user._id,
+        matchFilter,
+        'number items total_price status orderedAt completedAt',
+        options,
+      );
+
+      if (dbOrders.length) {
+        const count = await OrderService.countOrdersByUserId(
+          user?._id,
+          matchFilter,
+        );
+
+        routeService.resOk({ orders: dbOrders, count });
+      } else {
+        routeService.resNotFound();
+      }
+
+      return;
+    }
+
+    // Respond 401 Unauthorized if user is not authorized
+    // Respond 403 Forbidden if user doesn't have required permissions
+    if (!routeService.isAuthorized({ isManager: true })) return;
+
+    // get user orders by email
+    if (search && byUserEmail) {
+      // ? i think this one is not used
+      // respond 400 Validation Error if the name is invalid
+      // Respond 400 Validation Error if user doesn't exist
+      const dbUser = await UserService.findUser(search, '_id');
+
+      const dbOrders = await OrderService.listOrdersByUserId(
+        dbUser._id,
+        matchFilter,
+        null,
+        options,
+      ).populate({
+        path: 'user',
+        select: 'name email',
+      });
+
+      const { user } = routeService.getUserSession();
+
+      if (dbOrders.length) {
+        const count = await OrderService.countOrdersByUserId(
+          user._id,
+          matchFilter,
+        );
+
+        routeService.resOk({ orders: dbOrders, count });
+      } else {
+        routeService.resNotFound();
+      }
+
+      return;
+    }
+
+    // get order by its number
+    if (search) {
+      const dbOrder = await OrderService.queryOrder(search);
+
+      if (dbOrder) {
+        routeService.resOk({ order: dbOrder });
+      } else {
+        routeService.resNotFound();
+      }
+
+      return;
+    }
+
+    // get orders with matched filters
+    const dbOrders = await OrderService.listOrders(
+      matchFilter,
+      null,
+      options,
+    ).populate({
+      path: 'user',
+      select: 'name email',
+    });
+
+    if (dbOrders.length) {
+      const count = await OrderService.countOrders(matchFilter);
+
+      routeService.resOk({ orders: dbOrders, count });
+    } else {
+      routeService.resNotFound();
+    }
+  } catch (error) {
+    routeService.handleApiError(error);
+  }
+};
